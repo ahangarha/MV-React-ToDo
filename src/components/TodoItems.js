4eb9e72da951935b031ccf40b93a230a5ae1b075
Mostafa Ahangarha <ahangarha@riseup.net>
2022-03-21 19:31:38 +0300

    fix linter issues

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import TodoItem from './TodoItem'
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
 function TodoItems({ todos, toggleCompletion, deleteTodo }) {
   if (todos.length) {
     return (
       <ul id="todoList">
         {
-          todos.map((todo) => <TodoItem
-            todo={todo}
-            key={todo}
-            toggleCompletion={toggleCompletion}
-            deleteTodo={deleteTodo}
-          />)
+          todos.map((todo) => (
+            <TodoItem
+              todo={todo}
+              key={todo}
+              toggleCompletion={toggleCompletion}
+              deleteTodo={deleteTodo}
+            />
+          ))
         }
       </ul>
     );
@@ -20,15 +22,21 @@ function TodoItems({ todos, toggleCompletion, deleteTodo }) {
 
   return (
     <div>No todo yet! Add some...</div>
-  )
+  );
 }
 
 TodoItems.propTypes = {
-  todos: PropTypes.array,
-}
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    index: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  })),
+  toggleCompletion: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
 
 TodoItems.defaultProps = {
   todos: [],
-}
+};
 
-export default TodoItems
+export default TodoItems;
